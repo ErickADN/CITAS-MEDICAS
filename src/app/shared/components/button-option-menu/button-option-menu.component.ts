77ae10eb86250a3df418fad19f627d26d642dddf
ErickADN <erickalexandernoriegadionisio@gmail.com>
2025-12-19 01:00:36 -0500

    Menu de opciones de usuarios

--- src/app/shared/components/button-option-menu/button-option-menu.component.ts
+++ src/app/shared/components/button-option-menu/button-option-menu.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component ,Input } from "@angular/core";
+
 
 @Component({
   selector: 'app-button-option-menu',
   templateUrl: './button-option-menu.component.html',
   styleUrls: ['./button-option-menu.component.css']
 })
-export class ButtonOptionMenuComponent {
+export class ButtonOptionMenuComponent  {
 
+  @Input() title?:string;
+  @Input() description?: string;
+  @Input() icon?: string;
+  @Input() color?:string;
+  @Input() route?: string;
 }
